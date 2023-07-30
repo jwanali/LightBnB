dd09de5bd8c9952f59b40d62d8662dcccbafd44e
jwanali <ciwan_ali@hotmail.com>
2023-07-30 00:31:52 -0500

    LighthouseBnB Properties Challenge

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -111,15 +111,64 @@ const getAllReservations = function (guest_id, limit = 10) {
  */
 
 const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
+   // 1
+   const queryParams = [];
+   // 2
+   let queryString = `
+   SELECT properties.*, avg(property_reviews.rating) as average_rating
+   FROM properties
+   JOIN property_reviews ON properties.id = property_id
+   `;
+ 
+   // 3
+   if (options.city) {
+     queryParams.push(`%${options.city}%`);
+     queryString += ` WHERE city LIKE $${queryParams.length} `;
+   }
+
+   if(options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += ` AND owner_id = $${queryParams.length} `
+   }
+
+   if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night);
+    queryString += ` AND cost_per_night >= $${queryParams.length} `;
+    queryParams.push(options.maximum_price_per_night);
+    queryString += ` AND cost_per_night <= $${queryParams.length} `;
+   } else if (options.minimum_price_per_night && !optionmaximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night);
+    queryString += ` AND cost_per_night >= $${queryParams.length} `;
+   } else if (!options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night);
+    queryString += ` AND cost_per_night <= $${queryParams.length} `;
+   }
+
+
+   if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += ` AND property_reviews.rating >= $${queryParams.length} `;
+   }
+ 
+   // 4
+   queryParams.push(limit);
+   queryString += ` 
+   GROUP BY properties.id
+   ORDER BY cost_per_night
+   LIMIT $${queryParams.length};
+   `;
+ 
+   // 5
+   console.log(queryString, queryParams);
+ 
+   // 6
+   return pool
+    .query(queryString, queryParams)
+    .then((res) => res.rows)
+    .catch(err => {
+      console.log(err.message)
     });
+
 };
 
 
